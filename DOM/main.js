50a02e7c19e74918f93507fe84e00d65fffe5679
chaithanyakumar47 <kumarchaithanya.1@gmail.com>
2023-09-30 02:08:10 +0530

    New changes

--- DOM/main.js
+++ DOM/main.js
@@ -6,14 +6,33 @@ function storeDetails(e){
     e.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
 
     const userDetails = {
         userName: name,
-        userEmail: email
+        userEmail: email,
+        userPhone: phone
     };
 
     const userDetails_serialised = JSON.stringify(userDetails);
 
     localStorage.setItem('userDetails',userDetails_serialised);
+
+    const div = document.createElement('div');
+    div.setAttribute('id','extra');
+    div.appendChild(document.createTextNode(name));
+    div.appendChild(document.createTextNode(' - '));
+    div.appendChild(document.createTextNode(email));
+    div.appendChild(document.createTextNode(' - '));
+    div.appendChild(document.createTextNode(phone));
+
+    const ul = document.querySelector('ul');
+    const section = document.querySelector('.container');
+
+    section.insertBefore(div,ul);
+    console.log(userDetails.userEmail.value)
+
+
     
 }
+
