6e9129a3243a5695a08c09e555b20c4472e66bfb
chaithanyakumar47 <kumarchaithanya.1@gmail.com>
2023-12-01 22:15:36 +0530

    The server now sends responses back to the client

--- DOM/Backend Development/first-app.js
+++ DOM/Backend Development/first-app.js
@@ -3,7 +3,30 @@ const http = require('http');
 
 
 const server = http.createServer((req, res) => {
-    console.log('Chaithanya');
-});
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    if(req.url == '/home'){
+        res.write('<html>');
+        res.write('<head><title>My first Page</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+    }else if(req.url == '/about'){
+        res.write('<html>');
+        res.write('<head><title>My first Page</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+    }else if(req.url == '/node'){
+        res.write('<html>');
+        res.write('<head><title>My first Page</title></head>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+    }else{
+        res.write('<html>');
+        res.write('<head><title>My first Page</title></head>');
+        res.write('<body><h1>This is a test response from your Node.js server</h1></body>');
+        res.write('</html>');
+    }
+    res.end();
+    });
 
 server.listen(4000);
