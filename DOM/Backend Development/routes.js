1b315996e3054d920974084d5e3a6adc254ba663
chaithanyakumar47 <kumarchaithanya.1@gmail.com>
2023-12-02 23:08:42 +0530

    installed nodemon package into my project

--- DOM/Backend Development/routes.js
+++ DOM/Backend Development/routes.js
@@ -42,7 +42,7 @@ const requestHandler = ((req, res) =>{
       });
     } else {
       res.write('<html>');
-      res.write('<head><title>My First Page</title></head>');
+      res.write('<head><title>My First Page!</title></head>');
       res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
       res.write('</html>');
       res.end();
