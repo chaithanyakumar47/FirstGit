7623798f4384dfac5e19316f2a1cd931322d675b
chaithanyakumar47 <kumarchaithanya.1@gmail.com>
2023-12-04 19:18:30 +0530

    installed Express.js and simplifed the code.

--- DOM/Backend Development/app.js
+++ DOM/Backend Development/app.js
@@ -1,7 +1,17 @@
-const http = require('http');
 
-const routes = require('./routes')
+const express = require('express');
 
-const server = http.createServer(routes.handler);
+const app = express();
 
-server.listen(4000);
+app.use((req, res, next) =>{
+    console.log("In the middleware!!");
+    next();
+})
+
+app.use((req, res, next) =>{
+    console.log("In the next middleware!!");
+    res.send("<h1>Hey there!!</h1>");
+})
+
+
+app.listen(4000);
