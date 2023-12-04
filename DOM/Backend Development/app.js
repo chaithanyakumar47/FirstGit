86e6d199d0cdd3d148d578019cab8ce172ebcb0a
chaithanyakumar47 <kumarchaithanya.1@gmail.com>
2023-12-04 20:04:44 +0530

    Can now parse incoming request data.

--- DOM/Backend Development/app.js
+++ DOM/Backend Development/app.js
@@ -1,15 +1,28 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) =>{
-    console.log("In the middleware!!");
-    next();
+
+app.use(bodyParser.urlencoded({extended : false}));
+
+
+app.use('/add-Product',(req, res, next) =>{
+    
+    
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="quantity"><button type="submit">Add</button></form>');
+    
 })
 
-app.use((req, res, next) =>{
-    console.log("In the next middleware!!");
+app.post('/product', (req, res, next) =>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+
+app.use('/',(req, res, next) =>{
+    
     res.send("<h1>Hey there!!</h1>");
 })
 
